a8b0d9e759ffeac9e69473e63840f62e7f291b70
ntaokshaiyoa-afk <ntaokshaiyoa@gmail.com>
2026-04-16 20:47:39 +0900

    ビルド日時を取得

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,11 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   base: "./",
+  
+  define: {
+    __BUILD_DATE__: JSON.stringify(new Date().toLocaleString('ja-JP', { timeZone: 'Asia/Tokyo' })),
+    __COMMIT_HASH__: JSON.stringify(commitHash),
+  },
 
   worker: {
     format: "es"
