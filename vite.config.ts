3e357b18103d567003c7722c56ee9f141b9ee068
ntaokshaiyoa-afk <ntaokshaiyoa@gmail.com>
2026-04-16 20:54:43 +0900

    コミットハッシュ取得不具合修正

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from "vite";
+import { execSync } from 'node:child_process'
 const commitHash = execSync('git rev-parse --short HEAD').toString().trim()
 
 export default defineConfig({
