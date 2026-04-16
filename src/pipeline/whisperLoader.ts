c2b925114ab0a215625b2d4dcff1284e84c85d1b
ntaokshaiyoa-afk <ntaokshaiyoa@gmail.com>
2026-04-16 22:24:11 +0900

    Update whisperLoader.ts

--- src/pipeline/whisperLoader.ts
+++ src/pipeline/whisperLoader.ts
@@ -1,12 +1,15 @@
-// src/pipeline/whisperLoader.ts
-
 let Module: any;
 
 export async function loadWhisper() {
   if (Module) return Module;
 
-  // Emscriptenモジュール生成
-  Module = await (window as any).Module({
+  const ctx: any = self; // ★ここ重要
+
+  if (!ctx.Module) {
+    throw new Error("Module not found (WASM未ロード)");
+  }
+
+  Module = await ctx.Module({
     locateFile: (file: string) => {
       return `/assets/wasm/${file}`;
     }
