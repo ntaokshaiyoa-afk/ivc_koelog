7d49663bc1c2d312ae7ab98cdd7effdd788569fa
ntaokshaiyoa-afk <ntaokshaiyoa@gmail.com>
2026-04-20 09:17:40 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { execSync } from 'node:child_process'
 const commitHash = execSync('git rev-parse --short HEAD').toString().trim()
 
 export default defineConfig({
-  base: "./",
+  base: "/ivc_koelog/",
   
   define: {
     __BUILD_TIME__: JSON.stringify(new Date().toLocaleString('ja-JP', { timeZone: 'Asia/Tokyo' })),
