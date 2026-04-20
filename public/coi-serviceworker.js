36b5a605abc46196d0f220cf10d215b71735d987
ntaokshaiyoa-afk <ntaokshaiyoa@gmail.com>
2026-04-20 20:42:33 +0900

    Create coi-serviceworker.js

---
+++ public/coi-serviceworker.js
@@ -0,0 +1,16 @@
+if (typeof window === 'undefined') {
+  self.addEventListener('install', () => self.skipWaiting());
+  self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
+} else {
+  (async () => {
+    if (window.crossOriginIsolated) return;
+
+    const swUrl = new URL(location.href);
+    swUrl.pathname = '/coi-serviceworker.js';
+
+    const reg = await navigator.serviceWorker.register(swUrl);
+    await navigator.serviceWorker.ready;
+
+    location.reload();
+  })();
+}
